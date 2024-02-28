8514421a26e5f72384be2d563a4fedd3080e59a1
andrejspetersons <andrejpeterson1@gmail.com>
2024-02-28 11:28:54 +0200

    Flight Planner 2 28.02.24

---
+++ FlightPlanner/Controllers/AdminApiController.cs
@@ -0,0 +1,106 @@
+﻿using FlightPlanner.Models;
+using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace FlightPlanner.Controllers
+{
+    [Authorize]
+    [Route("admin-api")]
+    [ApiController]
+    public class AdminApiController : ControllerBase
+    {
+        private static readonly object _flightlocks = new object();
+        private readonly FlightStorage _flights;
+
+        public AdminApiController(FlightStorage flights)
+        {
+            _flights = flights;        
+        }
+
+        [HttpGet]
+        [Route("flights/{id}")]
+        public IActionResult GetFlight(int id)
+        {
+            var flight = _flights.GetFlightById(id);
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(flight);
+
+        }
+
+        [HttpPut]
+        [Route("flights")]
+        public IActionResult AddFlight(Flight flight)
+        {
+            lock (_flightlocks)
+            {
+                bool Exist = _flights.FlightExist(flight);
+
+                if (Exist)
+                {
+                    return Conflict();
+                }
+
+                if (flight == null)
+                {
+                    return BadRequest(flight);
+                }
+
+                if (
+                    string.IsNullOrEmpty(flight.Carrier) ||
+                    string.IsNullOrEmpty(flight.ArrivalTime) ||
+                    string.IsNullOrEmpty(flight.DepartureTime) ||
+                    string.IsNullOrEmpty(flight.From.City) ||
+                    string.IsNullOrEmpty(flight.From.Country) ||
+                    string.IsNullOrEmpty(flight.From.AirportCode) ||
+                    string.IsNullOrEmpty(flight.To.City) ||
+                    string.IsNullOrEmpty(flight.To.Country) ||
+                    string.IsNullOrEmpty(flight.To.AirportCode)
+                )
+                {
+                    return BadRequest(flight);
+                }
+
+
+
+                if (flight.From.isEqualAirport(flight.To))
+                {
+                    return BadRequest("Invalid airport");
+                }
+
+                if (DateTime.Parse(flight.ArrivalTime) <= DateTime.Parse(flight.DepartureTime))
+                {
+                    return BadRequest("Invalid date");
+                }
+
+                _flights.AddFlight(flight);
+                return Created($"Created flight {flight.Id}", flight);
+            }
+        }
+
+        [HttpDelete]
+        [Route("flights/{id}")]
+        public IActionResult DeleteFlight(int id)
+        {
+            lock (_flightlocks)
+            {
+                if (_flights.DeleteFlightById(id))
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+        }
+    }
+}
---
+++ FlightPlanner/Models/Flight.cs
@@ -0,0 +1,28 @@
+﻿namespace FlightPlanner.Models
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public Airport From { get; set; }
+        public Airport To { get; set; }
+        public string Carrier { get; set; }
+        public string DepartureTime { get; set; }
+        public string ArrivalTime { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            Flight other = (Flight)obj;
+            return this.Carrier == other.Carrier &&
+                   this.DepartureTime == other.DepartureTime &&
+                   this.ArrivalTime == other.ArrivalTime &&
+                   this.From.AirportCode.Trim().ToLower() == other.From.AirportCode.Trim().ToLower() &&
+                   this.To.AirportCode.Trim().ToLower() == other.To.AirportCode.Trim().ToLower();
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, From,To,Carrier,DepartureTime,ArrivalTime);  
+        }
+    }
+}
